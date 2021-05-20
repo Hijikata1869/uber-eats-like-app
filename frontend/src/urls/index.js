b227f9a6fac24b50801de647dbe8f066f6fada40
Hijikata1869 <liv.knk@gmail.com>
2021-05-20 22:09:20 +0900

    APIを呼ぶ関数を作成

---
+++ frontend/src/urls/index.js
@@ -0,0 +1,6 @@
+const DEFAULT_API_LOCALHOST = 'http://localhost:3000/api/vi';
+
+export const restaurantsIndex = `${DEFAULT_API_LOCALHOST}/restaurants`;
+export const foodsIndex = (restaurantId) => `${DEFAULT_API_LOCALHOST}/restaurants/${restaurantsId}/foods`;
+export const lineFoods = `${DEFAULT_API_LOCALHOST}/line_foods/replace`;
+export const orders = `${DEFAULT_API_LOCALHOST}/orders`;
