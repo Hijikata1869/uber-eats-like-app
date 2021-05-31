7ce0b85421daf3131eb4d897936f529877ba6560
Hijikata1869 <liv.knk@gmail.com>
2021-06-01 05:13:36 +0900

    注文モーダルに数量選択ボタンを実装

--- frontend/src/containers/Foods.jsx
+++ frontend/src/containers/Foods.jsx
@@ -57,6 +57,10 @@ const ItemWrapper = styled.div`
   margin: 16px;
 `;
 
+const submitOrder = () => {
+  console.log('登録ボタンが押された！')
+}
+
 export const Foods = ({
   match
 }) => {
@@ -130,9 +134,21 @@ export const Foods = ({
           <FoodOrderDialog 
             food={state.selectedFood}
             isOpen={state.isOpenOrderDialog}
+            countNumber={state.selectedFoodCount}
+            onClickCountUp={() => setState({
+              ...state,
+              selectedFoodCount: state.selectedFoodCount + 1,
+            })}
+            onClickCountDown={() => setState({
+              ...state,
+              selectedFoodCount: state.selectedFoodCount - 1,
+            })}
+            onClickOrder={() => submitOrder()}
             onClose={() => setState({
               ...state,
               isOpenOrderDialog: false,
+              selectedFood: null,
+              selectedFoodCount: 1,
             })}
           />
       }
